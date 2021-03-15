66bdc626870c70057c324155075f0f9699c4c9b8
Praanto Samadder <praanto.samadder07@gmail.com>
2021-03-15 22:15:20 +0600

    Fixed slide-menu scroll issue on iOS and iPadOS

--- js/script.js
+++ js/script.js
@@ -10,10 +10,17 @@ hamburgerButton.onclick = () => {
     setTimeout(function() {
         article.classList.add("article-cap-height")
     }, 200)
+
+    // Make document unscrollable when slide-menu is in view
+    document.documentElement.style.position = "fixed"
 }
 
 slideMenu.onclick = () => {
     slideMenu.classList.remove("slide-menu-in")
     slideMenu.classList.add("slide-menu-out"); article.classList.remove("article-cap-height")
+
+    
+    // Remove fixed position when slide-menu is slid away from view
+    document.documentElement.style.position = "inherit"
 }
 
